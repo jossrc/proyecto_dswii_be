10874466a4c9b7fa00da855983cfd927a57d7e71
diegogu <diegogu@gmail.com>
2022-05-25 02:03:45 -0500

    Api Autores

---
+++ src/main/java/com/earthbook/proyecto_dswii_be/repository/AutorRepository.java
@@ -0,0 +1,9 @@
+package com.earthbook.proyecto_dswii_be.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.earthbook.proyecto_dswii_be.entity.Autor;
+
+public interface AutorRepository extends JpaRepository<Autor, Integer> {
+
+}
---
+++ src/main/java/com/earthbook/proyecto_dswii_be/service/AutorService.java
@@ -0,0 +1,11 @@
+package com.earthbook.proyecto_dswii_be.service;
+
+import java.util.List;
+
+import com.earthbook.proyecto_dswii_be.entity.Autor;
+
+public interface AutorService {
+	
+	public List<Autor> listar();
+
+}
