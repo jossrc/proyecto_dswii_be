81e86a01c2e1dab6800ae2e41919df06c67916b3
Gushiken <i201911188@cibertec.edu.pe>
2022-06-11 17:04:56 -0500

    CRUD Pais

---
+++ src/main/java/com/earthbook/proyecto_dswii_be/service/PaisService.java
@@ -0,0 +1,9 @@
+package com.earthbook.proyecto_dswii_be.service;
+
+import java.util.List;
+
+import com.earthbook.proyecto_dswii_be.model.dto.PaisDTO;
+
+public interface PaisService {
+	public List<PaisDTO> listar();
+}
